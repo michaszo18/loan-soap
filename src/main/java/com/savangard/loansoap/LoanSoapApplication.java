92c847c8fe1b6a80e997852e264d315616493dda
Michal Szostek <michal.szostek@savangard.com>
2021-06-29 18:46:49 +0200

    init

---
+++ src/main/java/com/savangard/loansoap/LoanSoapApplication.java
@@ -0,0 +1,13 @@
+package com.savangard.loansoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanSoapApplication.class, args);
+	}
+
+}
